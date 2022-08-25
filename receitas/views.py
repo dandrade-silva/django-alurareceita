259d131da79983748e6feaae6f4ae95f5d94d9d0
Danilo Andrade <dandrade0803@gmail.com>
2022-08-25 03:48:36 -0300

    Adicionando DB Postgree

--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import Receita
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    receitas = Receita.objects.all()
+
+    dados = {
+        'receitas': receitas
+    }
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
